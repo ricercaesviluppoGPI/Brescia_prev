442903c65d62dbac7eeaab9eba34fff0b7ab9af4
ricercaesviluppoGPI <101252321+ricercaesviluppoGPI@users.noreply.github.com>
2024-06-24 12:15:47 +0200

    Update Previsione.py

--- Previsione.py
+++ Previsione.py
@@ -258,12 +258,10 @@ ricoveri = st.checkbox(":hospital: Grafici ricoveri")
 if ricoveri:
     st.plotly_chart(fig_cardio)
     st.plotly_chart(fig_resp)
-
-accuratezza_cardio_resp = st.checkbox(":mag: Confronto tra previsione e valori reali di ricoveri da triage")
-
-if accuratezza_cardio_resp:
-    st.image("Valori effettivi e predetti cardiovascolare range lag 1 giorno", caption='Confronto per ricoveri per malattie cardiovascolari :anatomical_heart:', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="PNG")
-    st.image("Valori effettivi e predetti respiratorio ranges lag 1 giorno.jpg", caption='Confronto per ricoveri per malattie respiratorie :lungs:', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="PNG")
+    accuratezza_cardio_resp = st.checkbox(":mag: Confronto tra previsione e valori reali di ricoveri da triage")
+    if accuratezza_cardio_resp:
+        st.image("Valori effettivi e predetti cardiovascolare range lag 1 giorno.jpg", caption='Confronto per ricoveri per malattie cardiovascolari :anatomical_heart:', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="PNG")
+        st.image("Valori effettivi e predetti respiratorio ranges lag 1 giorno.jpg", caption='Confronto per ricoveri per malattie respiratorie :lungs:', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="PNG")
 
 # Mappa Brescia
 
